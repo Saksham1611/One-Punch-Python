bb6b0a63c65d25e48b04dc202dd163fc39e51149
Saksham1611 <Saksham1611@users.noreply.github.com>
2021-01-08 21:39:23 +0000

    deploy: 0ea16db999d9d15d1c379b5bf1c9c133e1fb38bc

--- assets/js/search.js
+++ assets/js/search.js
@@ -47,7 +47,7 @@ jtd.onReady = function(ready) {
 
 function initSearch() {
     var request = new XMLHttpRequest();
-    request.open('GET', '/py-DS-Tips/assets/js/search-data.json', true);
+    request.open('GET', '/One-Punch-Python/assets/js/search-data.json', true);
   
     request.onload = function(){
       if (request.status >= 200 && request.status < 400) {
